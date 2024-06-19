aa6bfa6529cf3f56fe3759d5f9f69cc7da4058ff
Andrew <102756686+PenumRas@users.noreply.github.com>
2024-06-19 16:29:50 +0300

    ready cache_fibonacci

--- HW1.py
+++ HW1.py
@@ -1,4 +1,20 @@
 def caching_fibonacci():
+    cache_list = {}
+
     def fibonacci(n):
-        pass
-    pass
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        elif n in cache_list:
+            return cache_list[n]
+        else:
+            result = fibonacci(n - 1) + fibonacci(n - 2)
+            cache_list[n] = result
+        return result
+
+    return fibonacci
+
+
+fib = caching_fibonacci()
+print(fib(10))
